5d6f8b0f2fb926b9232fc183be4b4152765a5de6
Samandar1418 <nabiyevs967@gmail.com>
2023-11-05 23:02:59 +0500

    dbcads

--- app.js
+++ app.js
@@ -1,25 +1,68 @@
-let inputElements = document.querySelectorAll('.inputs');
-let btn = document.getElementById('save');
+const username = document.getElementById ('username');
+const name = document.getElementById ('name');
+const surname = document.getElementById ('surname');
+const age = document.getElementById ('age');
+const gender = document.getElementById ('gender');
+const password = document.getElementById ('password');
+const repassword = document.getElementById ('repassword');
+const lang = document.querySelectorAll ('input.language');
+const btn = document.getElementById ('save');
 
-btn.addEventListener('click', function () {
-    let allInputsFilled = true;
-    let username = document.getElementById ('username');
-    let name = document.getElementById ('name');
-    let surname = document.getElementById ('surname');
-    let age = document.getElementById ('age');
-    let gender = document.getElementById ('gender');
-    let password = document.getElementById ('password');
-    for (let input of inputElements) {
-        if (!input.value) {
-            allInputsFilled = false;
-            break;
+function validate() {
+    if (!username.value){
+        username.style.outlineColor = 'red';
+        username.fokus ();
+        return
+    }
+    if (!name.value){
+        name.style.outlineColor = 'red';
+        name.fokus ();
+        return
+    }
+    if (!password.value){
+        password.style.outlineColor = 'red';
+        password.fokus ();
+        return
+    }
+    if (!name.value){
+        name.style.outlineColor = 'red';
+        name.fokus ();
+        return
+    }
+    if (password.value != repassword.value){
+        alert ('parollar bir xil kiritilmadi..');
+        password.value =  '';
+        repassword.value = '';
+    }
+}
+function createobjectuser(){
+    let user = {};
+    user.username = username.value;
+    user.name = name.value;
+    user.surname = surname.value;
+    user.age = age.value;
+    user.gender = gender.value;
+    user.password = password.value;
+    user.lang = [];
+   lang.forEach(el => {
+       if (el.checked){
+           user.lang.push(el.value);
+       }
+   });
+   let data = localStorage.getItem ( 'users') ? JSON.parse ( localStorage.getItem ('users')): [];
+   if (data.length){
+    data.forEach(el => {
+        if (el.username == user.username){
+         alert ('bunday foydalanuvchi mavjud');
+         username = '';
+         username.style.outlineColor = 'red';   
         }
-    }
-
-    if (allInputsFilled) {
-        let User = {username,name,surname,age,gender,password};
-        localStorage.setItem (username , JSON.stringify(User))
-    } else {
-        alert("Toliq royxatni to'ldiring");
-    }
-});
+        
+    });
+   }
+}
+btn.addEventListener ( 'click', function (){
+validate ();
+createobjectuser ();
+console.log(user);  
+ })
